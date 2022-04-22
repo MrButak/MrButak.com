c7416c318e3b43a66fab10342f90106301f81ac2
Matthew Spence <mspence5555@gmail.com>
2022-04-21 20:33:51 -0500

    working on displaying projects, added custom svg icons for project links

--- frontend/src/components/home/ProjectLinks.vue
+++ frontend/src/components/home/ProjectLinks.vue
@@ -1,14 +1,13 @@
 <template>
-<div class="project-link-wrapper-main">
+<!-- <div class="project-link-wrapper-main"> -->
     <div class="project-link-wrapper">
         <a><img class="project-icon" src="../../assets/icons/github-icon-text.svg" alt="github-icon"></a>
         <a><img class="project-icon" src="../../assets/icons/website-icon-text.svg" alt="github-icon"></a>
         <a><img class="project-icon" src="../../assets/icons/play-video-icon-text.svg" alt="github-icon"></a>
-        <div class="project-link-text">
-        </div>
+        
     </div>
     
-</div>
+<!-- </div> -->
 <!-- <video class="video-wrapper" ref="smVideoRef" controls muted autoplay>
     <source v-bind:src="sdDemoVideo" type="video/webm">
 </video>               -->
@@ -28,12 +27,9 @@ import { ref, onMounted, computed } from 'vue';
 <style scoped>
 /* light #E9C891  lighter #f0f0e4 orange #D06224 dark-rusty #AE431E olive #8A8635 */
 .project-icon {
-    width: 100px;
+    width: 70px;
 }
-.project-link-wrapper-main {
-    display: flex;
-    flex-direction: column;
-}
+
 .project-link-wrapper {
     display: flex;
     align-items: center;
@@ -41,9 +37,7 @@ import { ref, onMounted, computed } from 'vue';
     width: 100%;
     padding: 10px 0 0 0;
 }
-.project-link-text {
-    display: flex;
-}
+
 /*Tablet sizes---------------------------------------------------------*/
 @media (min-width: 480px) {
 .project-link-wrapper {
