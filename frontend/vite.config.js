477e0597978e317600d64ac1a60b959cd8e67eda
MrButak <mspence5555@gmail.com>
2023-02-19 11:02:35 -0600

    Changed default Vite port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    server: { 
+        port: 2400,
+        strictPort: true,
+    },
+    preview: {
+        // https: true,
+        port: 2400,
+        strictPort: true,
+    },
 })
